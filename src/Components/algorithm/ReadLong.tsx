b389ffe3751d229c01fa3315371b47586ad21708
ftden45 <ftfores640@gmail.com>
2023-09-22 18:37:02 +0700

    init project and read long fn

---
+++ src/Components/algorithm/ReadLong.tsx
@@ -0,0 +1,79 @@
+import ReadLongF from "../../functions/long_numbers/read_long";
+import {useState} from "react";
+import './ReadLong.css';
+
+function Section({ number } : { number: number}) {
+    return (
+        <div className="section-array">
+            {number}
+        </div>
+    )
+}
+function ListDebug({ list } : { list: any[]}) {
+    const listItems = list.map((value,index) => {
+            return (
+                <ListLongNumber key={index} list={value}/>
+            );
+        }
+    );
+    return (
+        <div className="list-array-values">{listItems}</div>
+    );
+}
+function ListLongNumber({ list } : { list: number[]}) {
+    //const numbers = props.list;
+    const listItems = list.map((value,index) => {
+        if (index == 0) {
+            return (
+                <span key={index} className="length-array">
+                    <Section  number={value}/>
+                </span>
+            );
+        } else {
+            return (
+                <Section key={index} number={value}/>
+            );
+        }
+    }
+);
+    return (
+        <div className="array-values">{listItems}</div>
+    );
+}
+
+export default function ReadLong({ title }: { title: string }) {
+    const [numberFinel, setNumberFinel] = useState<number[]>([]);
+
+    //const addFive = () => dispatch({ type: "setCount", value: state.count + 5 });
+    //const reset = () => dispatch({ type: "reset" });
+    const readDataForm = (e: React.ChangeEvent<any>): void => {
+
+        e.preventDefault();
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+        // You can pass formData as a fetch body directly:
+        //fetch('/some-api', { method: form.method, body: formData });
+
+        // Or you can work with it as a plain object:
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson.numberLong);
+        let arrayFinel = ReadLongF(formJson.numberLong.toString(),1000,()=>console.log());
+        //console.log(arrayFinel,'arrayFinel');
+        setNumberFinel([...arrayFinel[1]]);
+    }
+
+
+    return (
+        <div>
+            <h1>Read Long {title}</h1>
+            <form method="post" onSubmit={readDataForm}>
+                <input type="text" name="numberLong"/>
+
+                <ListDebug list={numberFinel}/>
+                {/*<button onClick={addFive}>Add 5</button>*/}
+                <button type="submit">Read long number</button>
+            </form>
+        </div>
+    );
+}
---
+++ src/functions/long_numbers/read_long.ts
@@ -0,0 +1,40 @@
+export default function ReadLongF( numberString: string, numberOsn: number = 1000,fn: (val:number[]) => void) : any{
+    //numberString = '2030851670';
+    //console.log(numberString,'numberString');
+    const numberLength = numberString.length;
+    //console.log(numberLength,'numberLength');
+    let A: number[] = [0];
+    let DebugA: any[] = [];
+    let i:number;
+    let j:number;
+    let osn:number = numberOsn;
+    let countZeros = osn.toString().split('').reduce((x, y) => {
+        let val = 0;
+        if (y === '0') val = 1;
+        return x + val;
+    }, 0);
+    console.log('countZeros',countZeros);
+    //debugger
+
+    for (j = 0;j < numberLength;j++) {
+        let item = [];
+        let char: string = numberString.charAt(j);
+        if (A[0] == 0 || A[A[0]] >= osn/10  ) {A.push(0);A[0]++;}
+        for(i = A[0];i>=1;i--) {
+            if (typeof A[A[0]] != 'undefined' && typeof A[i+1] != 'undefined')
+                A[i+1] = A[i+1] + Math.floor(A[i]*10 / osn);
+            A[i] = A[i]*10 % osn;
+        }
+        A[1] = A[1] + parseInt(char);
+        for(let k = 0; k <= A[0]; k++){
+            item.push(A[k])
+        }
+        DebugA.push(item);
+        //console.log(A,'A');
+        fn(A);
+    }
+    if (A[A[0]] == 0) A.pop();
+    console.log(A,'A Final:');
+    //console.log(DebugA,'DebugA:');
+    return [A,DebugA];
+}
